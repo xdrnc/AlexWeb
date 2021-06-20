535a85381db0223f96da80cb8d261872f9b5bf27
alexander\alexander <alexand312z@hotmail.com>
2021-06-20 17:30:53 +0800

    implement the solution to increase one digit for every request and only request new calculation of pi when certain decimal limit has been reached

--- back/index.js
+++ back/index.js
@@ -4,22 +4,24 @@ const cors = require('cors')
 const port = 2999
 
 const sunRadiusKM = 696340;
-var currentPiDecimal = 2;
 var LimitofPiDecimal = 10;
+var currentProcessedPi = "3.1"; //starting from 3.1 because I directly append with next element in the first call
+var currentPiDecimal = 1; // because starting from .1
+
 
 var forceCacheRefresh = false;
 class DataCache {
     constructor(fetchFunction, minutesToLive = 10) {
-      this.millisecondsToLive = minutesToLive * 60 * 1000;
+      //this.millisecondsToLive = minutesToLive * 60 * 1000;
       this.fetchFunction = fetchFunction;
       this.cache = null;
       this.getData = this.getData.bind(this);
-      this.resetCache = this.resetCache.bind(this);
+      //this.resetCache = this.resetCache.bind(this);
       this.isCacheExpired = this.isCacheExpired.bind(this);
       this.fetchDate = new Date(0);
     }
     isCacheExpired() {
-      return (forceCacheRefresh || (this.fetchDate.getTime() + this.millisecondsToLive) < new Date().getTime());
+      return (forceCacheRefresh);// || (this.fetchDate.getTime() + this.millisecondsToLive) < new Date().getTime());
     }
     getData() {
       if (!this.cache || this.isCacheExpired()) {
@@ -27,7 +29,7 @@ class DataCache {
 
         var processedData = this.fetchFunction(); // execute heavy method to calculate pi for example
         this.cache = processedData;
-        this.fetchDate = new Date(); // set to current time
+//        this.fetchDate = new Date(); // set to current time
         forceCacheRefresh = false;
 
         return processedData;
@@ -38,30 +40,36 @@ class DataCache {
         return this.cache; 
       }
     }
-    resetCache() { //alex: not used for now
-     this.fetchDate = new Date(0); //1970-01-01T00:00:00.000Z
-    }
 }
 
-// return the string of the current pi value with the length based on the current limit of decimal point
+
+//alextodo return the string of the current pi value with the length based on the current limit of decimal point
 calcPiValue = () => {
-  //alextodo
-  return 3.14;
+  console.log("call calc pi value, expect to call this at either the first request or after the decimal limit has been reached/renewed");
+  var currentPi = 3.1400012345678912345678912341324134;
+  // alextodo: 3.140001234567891undefinedundefinedundefinedundefinedundefined
+  //           3.141234567891235 limit from js for decimal point (https://www.w3schools.com/js/tryit.asp?filename=tryjs_numbers1)
+  // consider 
+  //var LimitofPiDecimal = 10; (consider this in the actual logic)
+  return currentPi.toString();
 }
 var piValue = new DataCache(calcPiValue);
 
 
-// increase index of string based on "currentPiDecimal" value
+// alextodo: increase index of string based on "currentPiDecimal" value
 piIncreaseDecimal = () => {
-  var currentPi = piValue.getData();
-
+  console.log("inc decimal call piValue getData");
+  let totalPiInString = piValue.getData().toString();
+  console.log("totalPiInString: ", totalPiInString);
+  currentProcessedPi = currentProcessedPi + totalPiInString[currentPiDecimal+2]; // the current .1 as the default starting point
+  // alextodo: 3.140001234567891undefinedundefinedundefinedundefinedundefined
+  return currentProcessedPi;
 }
 
 
 app.get('/calculate', cors(), (req, res) => {
-//  console.log("current decimal: ", currentPiDecimal);
   var result = {
-      pi: piValue.getData(),
+      pi: piIncreaseDecimal(),
       sunCircumferenceKM: 2 * 3.14 * sunRadiusKM
   };
   
@@ -69,11 +77,9 @@ app.get('/calculate', cors(), (req, res) => {
   if(currentPiDecimal == LimitofPiDecimal)
   {
     LimitofPiDecimal += 10;
-    //refresh cache
     forceCacheRefresh = true;
   }
 
-//  console.log("limit of pi decimal: ", LimitofPiDecimal);
   res.json(result);
 })
 
